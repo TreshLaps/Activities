d34788253c982769f8fba80a8286caae7736c736
mathiamo <mathiamo@gmail.com>
2021-03-30 13:24:37 +0200

    remove unused

--- Activities.Web/ClientApp/src/components/Layout.tsx
+++ Activities.Web/ClientApp/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from 'styled-components';
 const LayoutContainer = styled.div`
     margin: 0 auto;

4bd1e91f337db098e34afc704a8272b1c66bd462
Tore Lervik <tore.lervik@bekk.no>
2021-04-04 23:12:08 +0200

    Added progress page as default home view

--- Activities.Web/ClientApp/src/App.tsx
+++ Activities.Web/ClientApp/src/App.tsx
@@ -3,7 +3,6 @@ import { Route } from 'react-router';
 import HomePage from './components/home/HomePage';
 import ActivitiesPage from './components/activities/ActivitiesPage';
 import IntervalsPage from './components/intervals/IntervalsPage';
-import ProgressPage from './components/progress/ProgressPage';
 import RacesPage from './components/races/races';
 import Layout from './components/Layout';
 
@@ -13,6 +12,5 @@ export default () => (
         <Route exact path="/activities" component={ActivitiesPage}/>
         <Route exact path="/intervals" component={IntervalsPage}/>
         <Route exact path="/races" component={RacesPage}/>
-        <Route exact path="/progress" component={ProgressPage}/>
     </Layout>
 );
--- Activities.Web/ClientApp/src/components/home/HomePage.tsx
+++ Activities.Web/ClientApp/src/components/home/HomePage.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProgressPage from '../progress/ProgressPage';
 
-const HomePage: React.FC = () => {
+const HomePage: React.FC = () => {    
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (isAuthenticated === true) {
+            return;
+        }
+
+        fetch(`/api/Authentication/IsAuthenticated/`)
+            .then(response => response.json() as Promise<boolean>)
+            .then(data => {
+                setIsAuthenticated(data);
+            })
+            .catch(error => setIsAuthenticated(false));
+    });
+
+    if (!isAuthenticated) {
+        return null;
+    }
+
     return (
-        <div>Hello world</div>
+        <div>
+            <ProgressPage/>
+        </div>
     );
 }
     

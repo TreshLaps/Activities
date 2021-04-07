3527e2e25aeea99576529849622f3e88ddc83ceb
Tore Lervik <tore.lervik@bekk.no>
2021-04-07 08:51:26 +0200

    Added ignored controlled to git

---
+++ Activities.Web/Pages/Debug/DebugController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Activities.Core.Caching;
+using Activities.Strava.Authentication;
+using Activities.Strava.Endpoints;
+using Activities.Strava.Endpoints.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Activities.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [StravaAuthenticationFilter]
+    public class DebugController : ControllerBase
+    {
+        private readonly ActivitiesClient _activitiesClient;
+        private readonly ICachingService _cachingService;
+        private readonly IPermanentStorageService _permanentStorageService;
+
+        public DebugController(ActivitiesClient activitiesClient, ICachingService cachingService, IPermanentStorageService permanentStorageService)
+        {
+            _activitiesClient = activitiesClient;
+            _cachingService = cachingService;
+            _permanentStorageService = permanentStorageService;
+        }
+
+        [HttpGet("ClearData")]
+        public async Task<IActionResult> ClearData()
+        {
+            var stravaAthlete = await HttpContext.TryGetStravaAthlete();
+            var activities = await _activitiesClient.GetActivities(stravaAthlete.AccessToken, stravaAthlete.AthleteId);
+            _cachingService.Remove($"ActivitiesCache:{stravaAthlete.AthleteId}");
+
+            foreach (var activity in activities)
+            {
+                _cachingService.Remove($"DetailedActivity:{activity.Id}");
+            }
+            
+            return Ok();
+        }
+        
+        [HttpGet("Save/{activityId}")]
+        public async Task<IActionResult> Save(long activityId)
+        {
+            var activity = await _permanentStorageService.Get<DetailedActivity>($"DetailedActivity:{activityId}");
+            return Content(JsonConvert.SerializeObject(activity), "application/json");
+        }
+    }
+}
